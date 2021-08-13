f9e86263ad5afa26a9e3bbe01fc63d3dba1df3f1
GreyRobin <86514645+GreyRobin@users.noreply.github.com>
2021-08-13 16:47:37 +1000

    Add files via upload

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -28,7 +28,14 @@ class PlayerArcher {
         angle -= 0.01;
         Matter.Body.setAngle(this.body, angle);
       }
-      
+      if (this.body.velocity.x > 0 && this.body.position.x > 400) { 
+        var position = [this.body.position.x, this.body.position.y]; 
+        this.trajectory.push(position); 
+      } 
+      for (var i = 0; i < this.trajectory.length; i++) { 
+        fill("white"); 
+        ellipse(this.trajectory[i][0], this.trajectory[i][1], 5, 5); 
+      }
       push();
       translate(pos.x, pos.y);
       rotate(angle);
